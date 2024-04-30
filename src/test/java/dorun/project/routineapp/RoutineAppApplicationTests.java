fb3e01c8de5713ab0e6bd3e0b47a3335420e9a07
U-keun Song <sukeun319@gmail.com>
2024-04-30 21:20:58 +0900

    first commit

---
+++ src/test/java/dorun/project/routineapp/RoutineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dorun.project.routineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
